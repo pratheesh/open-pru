b4a39224e805e24576e1c49a0872695841fd3c87
Nick Saulnier <nsaulnier@ti.com>
2026-03-30 20:05:54 -0500

    academy: Port intc/intc_mcu to AM243x, AM64x
    Port academy/intc/intc_mcu to all processors in the OpenPRU repo that support
    initializing the PRU cores from an MCU+ core.
    
    This is a test of new documentation to guide AI agents to perform specific
    actions in the OpenPRU project. This project tested the action
    "port an existing project to other processors". The AI agent successfully
    recognized that the project could be ported to AM243x & AM64x, but not to
    AM62x, since TI only supports loading PRU cores from Linux A53 on AM62x.
    
    This code change was 100% written by claude code, though I manually inspected
    every modified file to ensure correctness.
    
    Passing build tests with 0 human intervention:
    * All core makefiles successfully build and clean from a top-level make
    * All example.projectspec files successfully import into CCS and build through
    CCS
    
    Signed-off-by: Nick Saulnier <nsaulnier@ti.com>

---
+++ academy/intc/intc_mcu/firmware/am64x-evm/icss_g0_rtu_pru0_fw/ti-pru-cgt/linker.cmd
@@ -0,0 +1,52 @@
+/*
+ * AM64x_RTU0.cmd
+ *
+ * Example Linker command file for linking assembly programs built with the TI-PRU-CGT
+ * on AM64x RTU0 cores
+ */
+
+/* Specify the System Memory Map */
+MEMORY
+{
+      PAGE 0:
+	/* 8 KB RTU Instruction RAM */
+	RTU_IMEM	: org = 0x00000000 len = 0x00002000
+
+      PAGE 1:
+	/* Data RAMs */
+	/* 8 KB PRU Data RAM 0; use only the first 4 KB for PRU0 and reserve
+	 * the second 4 KB for RTU0 and Tx_PRU0 */
+	PRU0_DMEM_0	: org = 0x00000000 len = 0x00001000
+	/* 8 KB PRU Data RAM 1; reserved completely for Slice1 cores - PRU1,
+	 * RTU1 and Tx_PRU1; do not use for any Slice0 cores */
+	PRU1_DMEM_1	: org = 0x00002000 len = 0x00001000
+	/* NOTE: Custom split of the second 4 KB of ICSS Data RAMs 0 and 1
+	 * split equally between the corresponding RTU and Tx_PRU cores in
+	 * each slice */
+	RTU0_DMEM_0	: org = 0x00001000 len = 0x00000800
+	TX_PRU0_DMEM_0	: org = 0x00001800 len = 0x00000800
+	RTU1_DMEM_1	: org = 0x00003000 len = 0x00000800
+	TX_PRU1_DMEM_1	: org = 0x00003800 len = 0x00000800
+
+      PAGE 2:
+	/* C28 needs to be programmed to point to SHAREDMEM, default is 0 */
+	/* 64 KB PRU Shared RAM */
+	PRU_SHAREDMEM	: org = 0x00010000 len = 0x00010000
+}
+
+/* Specify the sections allocation into memory */
+SECTIONS {
+
+	.text		>  RTU_IMEM, PAGE 0
+	.stack		>  RTU0_DMEM_0, PAGE 1
+	.bss		>  RTU0_DMEM_0, PAGE 1
+	.cio		>  RTU0_DMEM_0, PAGE 1
+	.data		>  RTU0_DMEM_0, PAGE 1
+	.switch		>  RTU0_DMEM_0, PAGE 1
+	.sysmem		>  RTU0_DMEM_0, PAGE 1
+	.cinit		>  RTU0_DMEM_0, PAGE 1
+	.rodata		>  RTU0_DMEM_0, PAGE 1
+	.rofardata	>  RTU0_DMEM_0, PAGE 1
+	.farbss		>  RTU0_DMEM_0, PAGE 1
+	.fardata	>  RTU0_DMEM_0, PAGE 1
+}
